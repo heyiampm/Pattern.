7646fa4b4b0cd674d610d0c539bf65249dda52d4
Pm Pranta <heyiampm@gmail.com>
2026-01-22 10:54:26 +0600

    Checkerboard Star Pattern

---
+++ Pattern Python/Day22.py
@@ -0,0 +1,10 @@
+#Checkerboard Star Pattern
+
+n = 8
+for i in range(n):
+    for j in range(n):
+        if (i + j) % 2 == 0:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
