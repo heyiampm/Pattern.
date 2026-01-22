df370b2dc2ddd669047f3d2da2798f63a884be13
Pm Pranta <heyiampm@gmail.com>
2026-01-22 10:52:04 +0600

    Unique Number Palindrome Pattern

---
+++ Pattern Python/Day19.py
@@ -0,0 +1,9 @@
+#Unique Number Palindrome Pattern
+
+n = 5
+for i in range(1, n+1):
+    for j in range(1, i+1):
+        print(j, end="")
+    for k in range(i-1, 0, -1):
+        print(k, end="")
+    print()
